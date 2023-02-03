d9489f4112e676a28982b5005189bb71656d875c
kimtaehyun98 <kimtaehyun98@naver.com>
2023-02-03 13:50:32 +0900

    feat(was): Request Header 출력하기 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,11 +3,9 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.Objects;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,6 +22,13 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line = br.readLine();
+            while(!(Objects.isNull(line) || line.equals(""))){
+                logger.info(line);
+                line = br.readLine();
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello world".getBytes();
             response200Header(dos, body.length);
